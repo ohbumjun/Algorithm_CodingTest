57fc748ff0d9a9e66123b74a864befb5bc61c520
OH BUM JUN <beomjun0638@hanmail.net>
2021-03-12 12:06:56 +0900

    Python Karatsuba Algorithm

---
+++ Algorithm/BOJ/백준강좌/1stAgain/스타트와링크_0312.py
@@ -0,0 +1,96 @@
+# https://www.acmicpc.net/status?user_id=dhsys112&problem_id=14889&from_mine=1
+
+import collections
+from collections import deque, Counter
+import sys
+import heapq
+sys.stdin = open("input.txt", "rt")
+sys.setrecursionlimit(100000)
+
+'''
+해당 문제는,
+순열로 풀수 있다.
+
+모든 사람을 순서대로 넣는 것이다.
+
+물론, 각 사람이 어디에 들어가는지는 모르지만
+2개의 집단으로
+그리고 각각 n // 2명의 사람들이 있는 집단으로
+나뉜다는 것은 알 수 있다
+
+0 ~ n - 1 이라는 idx를 각각의 사람이라고 해보자.
+b를 집단 배열이라고 하면
+b[i]는 i idx 사람이 속한 집단을 의미한다
+
+집단을 0과 1로 나눈다고 해보자.
+
+1) next_permutation 사용을 위해, 최소 순열 초기화 후 시작
+2) 각 단계마다, b[i] == 1, b[i] == 0인 2개의 집단을 구분한다.
+
+각각을 first, second라는 집단에 i를 넣는다( 서로 다른 idx 집단이 있을 것이다 )
+
+매 단계마다
+first, second 집단의 합을 구하고
+그 차이를 구한다
+
+그 차이의 최소를 구한다.
+
+'''
+
+
+def next_permutation(a):
+    i = len(a) - 1
+    while i > 0 and a[i-1] >= a[i]:
+        i -= 1
+    if i <= 0:
+        return False
+
+    j = len(a) - 1
+
+    while a[j] <= a[i-1]:
+        j -= 1
+
+    a[j], a[i-1] = a[i-1], a[j]
+
+    j = len(a) - 1
+
+    while i < j:
+        a[i], a[j] = a[j], a[i]
+        i += 1
+        j -= 1
+
+    return True
+
+
+n = int(input())
+b = [0 if i < n // 2 else 1 for i in range(n)]
+
+arr = [list(map(int, input().split())) for _ in range(n)]
+res = 21412389713
+
+while True:
+    first = []
+    one = 0
+    second = []
+    two = 0
+
+    for i in range(n):
+        if b[i] == 0:
+            first.append(i)
+        else:
+            second.append(i)
+
+    # 합 구하기
+    for i in range(n//2):
+        for j in range(n//2):
+            if i == j:
+                continue
+            one += arr[first[i]][first[j]]
+            two += arr[second[i]][second[j]]
+
+    res = min(res, abs(one - two))
+
+    if not next_permutation(b):
+        break
+
+print(res)
--- Algorithm/BOJ/백준강좌/2ndAgain/연산자끼워넣기_재귀_0301.py
+++ Algorithm/BOJ/백준강좌/2ndAgain/연산자끼워넣기_재귀_0301.py
@@ -1,16 +1,19 @@
 # https://www.acmicpc.net/problem/14888
 
+import collections
+from collections import deque, Counter
 import sys
 import heapq
 sys.stdin = open("input.txt", "rt")
-from collections import deque, Counter
-import collections
 sys.setrecursionlimit(100000)
 
 '''
-
+< 재귀 >
 여기서 중요한 점은,
 연산자를 어디에 넣는지이다.
+
+즉, 여기서도 마찬가지로, 
+모든 연산자를 넣어야 하는데, 그 순서를 바꿔서 넣어야 하는 경우인 것이다
 
 그렇다면, 모든 연산자를 다 넣어야 하는건가 ?
 너무 많은 경우의 수가 있으면 어떡하지 ?
@@ -33,9 +36,15 @@ sys.setrecursionlimit(100000)
 
 10C2 * 8C2 * 6C3 * 3C3 = !
 이와 같은 과정을 겪으면 된다.
+왜 ?? 
+10개가 들어갈 수 있는 공간 중에서
++ 2개가 들어갈 공간 선택
+
+이후 8개가 들어갈 수 있는 공간 중에서
+- 2개가 들어갈 공간 선택
 
 그리고 실제 이 경우,
-4 ^ 10 보다는 훨씬 더 적은 경우의 수를 얻을 수 있다.
+4 ^ 10 보다는 훨씬 더 적은 경우의 수를 얻을 수 있다
 
 다음과 같은 식을 사용하여 구해보자
 go( arr , idx, cur, plus, minus, mul, div )
@@ -62,45 +71,45 @@ res라는 임의의 배열을 두고
 
 '''
 
-n      = int(input())
-numArr = list(map(int,input().split()))
-calNum = list(map(int,input().split()))
+n = int(input())
+numArr = list(map(int, input().split()))
+calNum = list(map(int, input().split()))
 
 resMin = 123242132
 resMax = 0
 resArr = []
 
-def go( arr, idx, cur,  plus, minus, mul, div ) :
+
+def go(arr, idx, cur,  plus, minus, mul, div):
 
     n = len(arr)
-    
+
     if idx == n:
-        return (cur,cur)
-    
+        return (cur, cur)
+
     res = []
-    if plus > 0 :
-        res.append(go( arr, idx + 1, cur + arr[idx],  plus - 1, minus, mul, div ))
-    if minus > 0 :
-        res.append(go( arr, idx + 1, cur - arr[idx],  plus, minus - 1, mul, div ))
-    if mul > 0 :
-        res.append(go( arr, idx + 1, cur * arr[idx],  plus, minus, mul - 1, div ))
-    if div > 0 :
-        if cur >= 0 :
-            res.append(go( arr, idx + 1, cur // arr[idx ], plus, minus, mul, div -1 ))
-        else :
-            res.append(go( arr, idx + 1, - ( cur // arr[idx ] ), plus, minus, mul, div -1 ))
-    ans = ( max([t[0] for t in res]) , min([t[1] for t in res]) )
+    if plus > 0:
+        res.append(
+            go(arr, idx + 1, cur + arr[idx],  plus - 1, minus, mul, div))
+    if minus > 0:
+        res.append(
+            go(arr, idx + 1, cur - arr[idx],  plus, minus - 1, mul, div))
+    if mul > 0:
+        res.append(
+            go(arr, idx + 1, cur * arr[idx],  plus, minus, mul - 1, div))
+    if div > 0:
+        if cur >= 0:
+            res.append(
+                go(arr, idx + 1, cur // arr[idx], plus, minus, mul, div - 1))
+        else:
+            res.append(
+                go(arr, idx + 1, - (cur // arr[idx]), plus, minus, mul, div - 1))
+    ans = (max([t[0] for t in res]), min([t[1] for t in res]))
 
     return ans
 
-    
 
-ans = go( numArr , 1, numArr[0],  calNum[0], calNum[1], calNum[2], calNum[3] )
+ans = go(numArr, 1, numArr[0],  calNum[0], calNum[1], calNum[2], calNum[3])
 
 print(ans[0])
 print(ans[1])
-
-        
-            
-            
-        
