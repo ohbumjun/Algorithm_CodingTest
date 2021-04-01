8e359bf1f387faecc1dba46cf26f61b666310143
OH BUM JUN <beomjun0638@hanmail.net>
2021-04-01 16:03:28 +0900

    백준 연구소

--- Algorithm/BOJ/python/1stAgain/뱀과사다리게임_0328.py
+++ Algorithm/BOJ/python/1stAgain/뱀과사다리게임_0328.py
@@ -50,7 +50,7 @@ ex) 3 -> 5 -> 48
 from collections import deque
 
 n,  m = map(int, input().split())
-after = list(range(10))
+after = list(range(101))
 dist = [-1] * 101
 for _ in range(n + m):
     x, y = map(int, input().split())
@@ -84,3 +84,189 @@ while q:
             dist[y] = dist[x] + 1
             q.append(y)
 print(dist[100])
+
+'''
+C++
+
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#define next _next
+using namespace std;
+int dist[101];
+int next[101];
+int main() {
+    int n, m;
+    cin >> n >> m;
+    for (int i=1; i<=100; i++) {
+        next[i] = i;
+        dist[i] = -1;
+    }
+    while (n--) {
+        int x, y;
+        cin >> x >> y;
+        next[x] = y;
+    }
+    while (m--) {
+        int x, y;
+        cin >> x >> y;
+        next[x] = y;
+    }
+    dist[1] = 0;
+    queue<int> q;
+    q.push(1);
+    while (!q.empty()) {
+        int x = q.front(); q.pop();
+        for (int i=1; i<=6; i++) {
+            int y = x+i;
+            if (y > 100) continue;
+            y = next[y];
+            if (dist[y] == -1) {
+                dist[y] = dist[x] + 1;
+                q.push(y);
+            }
+        }
+    }
+    cout << dist[100] << '\n';
+    return 0;
+}
+
+'''
+
+
+# 문제가 되는 코드 1
+# 뱀, 사다리 > next[x] = y
+# 일반 칸 : next[x] = -1
+# 이렇게 저장하고 나면, 어떤 문제가 발생할까 ?
+
+while(!q.empty()){
+    int x = q.front(); q.pop();
+    for(int i=0; i <= 6; i++){
+        int y = x + i;
+        if(y > 100): continue;
+
+        # y가 뱀의 시작 혹은 사다리의 시작
+        # 뱀 혹은 사다리의 끝을 방문한 적이 없을 때
+        if(next[y] != -1 & & dist[next[y]] == -1){
+            # 뱀 또는 사다리를 타고 이동한다
+            y = next[y]
+        }
+        if(dist[y] == -1){
+            # 방문하지 않았다면, 방문하는 것이다
+            dist[y] = dist[x] + 1;
+            q.push(y)
+        }
+    }
+}
+
+'''
+if(next[y] != -1 && dist[next[y]] == -1){
+    # 뱀 또는 사다리를 타고 이동한다
+    y = next[y]
+}
+
+위의 경우가 문제가 된다. 
+현재 3에 위치하여 있고
+5 -> 7 에 있는 경우를 생각해보자 
+
+7은 이미 방문한 적이 있을 수도 있다
+2 -> 7
+
+따라서, 3에서 5로 이동할때, 
+5 -> 7 로의 이동 경우가 고려되지 않을 수도 있다
+
+왜냐하면, 7은 이미 방문처리되어
+5 -> 7 경우가 고려되지 않을 수도 있다
+
+dist[next[y]] == -1
+라는 코드를 넣은 이유가 ,
+
+무언가 BFS이니까
+방문처리를 해줘야 할 것 같아서
+위와 같이, 처리해준 것일 수도 있다 
+
+그런데, 사실
+게임 관점으로 보면
+x -> next[y]로 바로 간 것이다
+최종 도착 상태가 next[y]니까
+
+즉, 중간에 y를 들렸는지 안들렸는지는
+사실 중요하지 않은 것이다
+
+헷갈릴 수 있는데 ,
+dist(next[y])라는 코드에는
+'y'라는 요인이 고려되는 것인데,
+그러면 안된다는 것이다 
+
+정리하자면,
+최종점이 아니면
+방문하면 안된다 
+
+'''
+
+# 문제가 되는 코드 2
+while(!q.empty()){
+    int x = q.front(); q.pop();
+    for(int i=0; i <= 6; i++){
+        int y = x + i;
+        if(y > 100): continue;
+        # y가 뱀의 시작 혹은 사다리의 시작
+        # 뱀 혹은 사다리의 끝을 방문한 적이 없을 때
+        if(next[y] != -1 & & dist[next[y]] == -1){
+            # 뱀 또는 사다리를 타고 이동한다
+            dist[next[y]] = dist[x] + 1;
+
+            # 중간정점도 방문하고, 거리 계산해준다
+            # !! 문제다. 최종점이 아니면, 방문처리를 해주어서는 안되는데
+            # 여기에서 방문처리를 해줘버렸다
+            dist[y] = dist[x] + 1;
+            q.push(next[y])
+        }else{
+            # 일반 칸일 때
+            if(dist[y] == -1) dist[y] = dist[x] + 1;
+            q.push(y); }
+    }
+}
+
+'''
+언제 문제가 생기는 것일까 ??
+
+ex) 
+7 -> 8
+14 -> 4
+
+1에서 시작하여
+2, 3, 4, 5, 6, 7 을 간다고 하면
+결과적으로 7은 바로 8로 가기 때문에
+
+1 -> {2, 3, 4, 5, 6, 7}이 나오게 된다
+
+1 이 끝나고, 그 이후의 숫자들에 대해서
+방문을 처리하려고 한다고 해보자 
+2,3,4,5,6 같은 경우는 이미 위의 1에서 방문해버렸기 때문에
+3 -> 9
+4 -> 10
+5 -> 11
+6 -> 12
+
+다 돌아서, 
+8로 왔다고 해보자 
+8에서는 13만 방문할 수 있다
+왜 ? 최종 도착 칸외에는
+방문처리를 원래 해주어서는 안되기 때문이다 
+
+
+그런데, 코드상으로는 
+8 -> {13, 14} 가 가능하다 
+
+14는 가면 안된다
+사다리를 통해 4라는 곳으로
+최종 도착칸이 설정되기 때문이다
+
+하지만, 현재 코드상으로
+dist[next[14]] == dist[4] >> != -1 로 설정되어서
+if문에 걸리지 못하고
+else문에서 처리된다
+
+이러한 문제가 있다는 것이다 
+'''
---
+++ Algorithm/BOJ/백준강좌/1stAgain/연구소_0401.py
@@ -0,0 +1,240 @@
+# https://www.acmicpc.net/problem/14502
+
+'''
+해당 문제는 크게 2가지 부분으로 진행된다
+
+1) 벽을 3개 세우는 부분 
+> 2) 그리고 벽을 세울 때마다, 바이러스가 퍼질 수 없는 곳의 크기.
+    까지 구하는 부분
+
+총 2 부분이 존재하는 것이다
+
+
+< 벽을 3개 세우는 부분 == Brute Force > ---------------------------------------
+
+어디에 어떤 벽을 세우면, 바이러스가 얼마나 퍼지는지 '모른다'
+
+모르기 때문에 다 해봐야 한다
+
+즉, Brute Force 문제가 되는 것이다 . 
+
+< 시간 복잡도 >
+빈칸의 개수는 총 N * M 개 
+
+각 칸에는 3가지 벽을 세울 수 있다. 각 칸에 대해서 3가지 경우를 모두 고려한다고 하면
+
+O((N * M) ^ 3) 이라는 값을 얻을 수 있다. 
+
+
+< 벽을 세울 때마다, 바이러스가 퍼질 수 없는 곳의 크기 == BFS, DFS > -------------
+
+바이러스가 퍼질 수 없는 곳을 구하려면 ,
+
+" 빈칸 개수 - 바이러스가 퍼진 칸의 개수 "를 구하면 된다  
+
+' 바이러스가 퍼진 칸의 개수 ' 는 어떻게 구할 것인가 ??
+바로, DFS, BFS 를 통해 구할 수 있다  
+
+' 바이러스가 퍼진다 ' 라는 것은 ,
+'바이러스가 있는 곳 --> 주변 정점으로 계속 퍼져나가는' 과정을 생각하면 되는 것이다 
+
+< 시간 복잡도 >
+빈칸의 최대 개수 O( NM )
+
+
+결과적으로
+BF      : O((NM) ^ 3)
+DFS,BFS : O(NM)
+
+=> O((NM) ^ 4)
+
+N,M의 최댓값은, 64
+
+따라서, 시간복잡도는 ( 2^6 )^4 == 2^24
+
+1억 보다 작다
+따라서 충분히, 진행할 수 있다 
+
+'''
+import sys
+from collections import deque
+sys.stdin = open("input.txt", "rt")
+sys.setrecursionlimit(100000)
+
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+
+N, M = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+ans = 0
+
+
+def bfs():
+    queue = deque([])
+
+    # arr 정보를 bfsA 에 넣는다
+    bfsA = [[0] * M for _ in range(N)]
+    for i in range(N):
+        for j in range(M):
+            bfsA[i][j] = arr[i][j]
+            # 바이러스 찾으면 넣기
+            if bfsA[i][j] == 2:
+                queue.append((i, j))
+
+    # 바이러스에 대한 bfs 진행
+    res = 0
+
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < N and 0 <= ny < M and bfsA[nx][ny] == 0:
+                bfsA[nx][ny] = 2
+                queue.append((nx, ny))
+
+    # 바이러스의 개수 세기
+    for i in range(N):
+        for j in range(M):
+            if bfsA[i][j] == 0:
+                res += 1
+    return res
+
+
+# 3중 for문
+for x1 in range(N):
+    for y1 in range(M):
+        if arr[x1][y1] != 0:
+            continue
+        for x2 in range(N):
+            for y2 in range(M):
+                if arr[x2][y2] != 0:
+                    continue
+                for x3 in range(N):
+                    for y3 in range(M):
+                        if arr[x3][y3] != 0:
+                            continue
+
+                        # 같은 곳에 벽을 세우면 안된다
+                        if x1 == x2 and y1 == y2:
+                            continue
+                        if x1 == x3 and y1 == y3:
+                            continue
+                        if x2 == x3 and y2 == y3:
+                            continue
+
+                        # 이제 벽세우면 되는 건가
+
+                        arr[x1][y1] = 1
+                        arr[x2][y2] = 1
+                        arr[x3][y3] = 1
+
+                        cur = bfs()
+
+                        ans = max(ans, cur)
+
+                        arr[x1][y1] = 0
+                        arr[x2][y2] = 0
+                        arr[x3][y3] = 0
+
+print(ans)
+
+
+'''
+C++
+
+#include <iostream>
+#include <queue>
+using namespace std;
+int n, m;
+int a[10][10];  // 연구소 지도 
+int b[10][10];  // 연구소 지도 << BFS 시에만 사용한다 
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
+int bfs() {
+    // 시작 정점 넣어주기 
+    queue<pair<int,int>> q;
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) {
+            b[i][j] = a[i][j];
+            if (b[i][j] == 2) {
+                // 바이러스 발견해서 넣기 
+                q.push(make_pair(i,j));
+            }
+        }
+    }
+    while (!q.empty()) {
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        for (int k=0; k<4; k++) {
+            int nx = x+dx[k];
+            int ny = y+dy[k];
+            // 인접 4개 정점 + 범위 내 존재 
+            if (0 <= nx && nx < n && 0 <= ny && ny < m) {
+                // 빈칸이면, 바이러스 퍼뜨리기 
+                if (b[nx][ny] == 0) {
+                    b[nx][ny] = 2;
+                    q.push(make_pair(nx,ny));
+                }
+            }
+        }
+    }
+    int cnt = 0;
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) {
+            // 빈칸의 개수 세어주기  
+            if (b[i][j] == 0) {
+                cnt += 1;
+            }
+        }
+    }
+    return cnt;
+}
+int main() {
+    cin >> n >> m;
+    for (int i=0; i<n; i++) {
+        for (int j=0; j<m; j++) {
+            cin >> a[i][j];
+        }
+    }
+    int ans = 0;
+    // 6중 for문 >> Brute Force ( 재귀함수를 사용해도 된다)
+    // 3개의 칸을 세워야 하고, 행 + 열.을 탐색해야 하기 때문에, 아래와 같이 진행하였다 
+    for (int x1=0; x1<n; x1++) {
+        for (int y1=0; y1<m; y1++) {
+            if (a[x1][y1] != 0) continue; // 1번째 칸 : 벽은 빈칸에만 세울 것이다 
+            for (int x2=0; x2<n; x2++) {
+                for (int y2=0; y2<m; y2++) {
+                    if (a[x2][y2] != 0) continue; // 2번째 칸 : 벽은 빈칸에만 세울 것이다 
+                    for (int x3=0; x3<n; x3++) {
+                        for (int y3=0; y3<m; y3++) {
+                            if (a[x3][y3] != 0) continue; // 3번째 칸 : 벽은 빈칸에만 세울 것이다 
+                            if (x1 == x2 && y1 == y2) continue; // 3개의 벽을 세울 것이고, 같은 것이 하나라도 있으면 안된다 
+                            if (x1 == x3 && y1 == y3) continue;
+                            if (x2 == x3 && y2 == y3) continue;
+
+                            // bfs 전에 벽을 세워준다 
+                            a[x1][y1] = 1;
+                            a[x2][y2] = 1;
+                            a[x3][y3] = 1;
+
+                            // cur : 바이러스가 퍼질 수 없는 공간의 크기 
+                            int cur = bfs();
+                            // 최댓값 갱신 
+                            if (ans < cur) ans = cur;
+                            // 해당 경우에 대한 벽세우기 3번을 진행 , 다시 취소해주기 
+                            a[x1][y1] = 0;
+                            a[x2][y2] = 0;
+                            a[x3][y3] = 0;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    cout << ans << '\n';
+    return 0;
+}
+
+'''
