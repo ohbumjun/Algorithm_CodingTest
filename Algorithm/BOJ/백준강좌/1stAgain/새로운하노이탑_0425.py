db11ced5a4e768bf9619bca8dbc911613ede7ed7
OH BUM JUN <beomjun0638@hanmail.net>
2021-04-25 19:31:46 +0900

    백준 새로운 하노이탑

---
+++ Algorithm/BOJ/백준강좌/1stAgain/새로운하노이탑_0425.py
@@ -0,0 +1,138 @@
+# https://www.acmicpc.net/problem/12906
+# Python --
+from collections import deque
+
+# 시작 상태
+# s[i] : i번째 막대에 들어있는 원판
+# ex) s[0] : 0번째, 즉, A 막대에 들어있는 원판
+import sys
+import heapq
+import math
+from collections import deque
+sys.stdin = open("input.txt", "rt")
+sys.setrecursionlimit(1001*1001)
+
+# 시작 상태
+# s[i] : i번째 막대에 들어있는 원판
+# ex) s[0] : 0번째, 즉, A 막대에 들어있는 원판
+s = []
+for i in range(3):
+    temp = input().split()
+    cnt = int(temp[0])
+    if cnt > 0:
+        s.append(temp[1])
+    else:
+        s.append('')
+
+# 최종 완성 상태
+final = [0, 0, 0]
+for chrs in s:
+    for ch in chrs:
+        final[ord(ch) - ord('A')] += 1  # 각 막대에 몇개의 원판이 있어야 하는지 최종 상태 저장하기
+
+d = {}
+d[tuple(s)] = 0
+q = deque()
+q.append(tuple(s))
+while q:
+    now = q.popleft()
+    # i번째 막대에서, j번째 막대로 원판 보내기
+    for i in range(3):
+        for j in range(3):
+            if i == j:
+                continue  # 같은 막대 사이에서는 이동이 필요 없다
+            if len(now[i]) == 0:
+                continue  # 시작 막대에 원판이 없다면
+            # y는 2차원 배열 --> y[i] : i번째 막대를 의미한다
+            new = list(now[:])
+            new[j] += new[i][-1]
+            new[i] = new[i][:-1]
+            new = tuple(new)
+
+            if new not in d:
+                # 해당 막대 모양을 만들어준 적이 없다면, 추가해준다
+                d[new] = d[now] + 1
+                q.append(new)
+ans = ['', '', '']
+for i in range(3):  # 3개의 원판 및 막대에 대해서 조사한다
+    # final 이라는 숫자배열로 부터, 문자열 배열을 만들어낸다
+    for j in range(final[i]):
+        ans[i] += chr(ord('A') + i)
+
+print(d[tuple(ans)])
+
+
+# C++
+# include <iostream>
+# include <map>
+# include <queue>
+# include <array>
+'''
+using namespace std
+int main() {
+    array < string, 3 > s // 각 막대에 대한 정보
+    
+    // 처음 상태 세팅 
+    for (int i=0
+         i < 3
+         i++) {
+        int cnt
+        cin >> cnt
+        if (cnt > 0) {
+            cin >> s[i]
+        } else {
+            s[i] = ""
+        }
+    }
+    int cnt[3] = {0, 0, 0}
+    for (int i=0
+         i < 3
+         i++) {
+        for (int j=0
+             j < s[i].length()
+             j++) {
+            cnt[s[i][j]-'A'] += 1 // 최종상태를 확인하기 위해, 각 알파벳의 개수 넣어주기 
+        }
+    }
+    map < array < string, 3 >, int > d  // 정수형태의 정점이 아니므로, 별도의 자료형(여기서는 map)을 사용하는 것이다 
+    queue < array < string, 3 >> q
+    q.push(s)
+    d[s] = 0
+
+    // 모든 이동의 경우의 수 조사해주기 
+    while (!q.empty()) {
+        auto now = q.front()
+        q.pop()
+        // 각 막대를 시작점, 끝점으로 하여 이동시키는 모든 경우의 수 3*3 을 고려해준다 ( i 에서 j 로 )
+        for (int i=0
+             i < 3
+             i++) {
+            for (int j=0
+                 j < 3
+                 j++) {
+                if (i == j) continue
+                if (now[i].length() == 0) continue
+                array < string, 3 > next(now)
+                next[j].push_back(next[i].back())  // 제일 위에 있는 거 빼서, 옮겨주기 == 배열 상 가장 뒤에 있는 거 빼서 뒤로 넣어주기 
+                next[i].pop_back()
+                if (d.count(next) == 0) {
+                    d[next] = d[now] + 1
+                    q.push(next)
+                }
+            }
+        }
+    }
+    array < string, 3 > ans
+    for (int i=0
+         i < 3
+         i++) {
+        for (int j=0
+             j < cnt[i]
+             j++) {
+            ans[i] += (char)('A' + i)
+        }
+    }
+    cout << d[ans] << '\n'
+    return 0
+}
+'''
--- Algorithm/BOJ/백준강좌/1stAgain/성곽_0425.py
+++ Algorithm/BOJ/백준강좌/1stAgain/성곽_0425.py
@@ -1,78 +1,96 @@
 #
 
 # Python 풀이 -----
+import sys
+import heapq
+import math
 from collections import deque
+sys.stdin = open("input.txt", "rt")
+sys.setrecursionlimit(1001*1001)
+
 dx = [0, -1, 0, 1]
 dy = [-1, 0, 1, 0]
-m, n = map(int, input().split())
-a = [list(map(int, input().split())) for _ in range(n)]
-d = [[0]*m for _ in range(n)]
+
+n, m = map(int, input().split())  # m : 행, n : 열
+a = [list(map(int, input().split())) for _ in range(m)]
+group = [[-1]*n for _ in range(m)]
+group_size = []
+room = -1
+mSize = 0
 
 
-def bfs(x, y, rooms):
+def bfs(sx, sy, room):
+    group[sx][sy] = room
     q = deque()
-    q.append((x, y))
-    d[x][y] = rooms
-    cnt = 0
+    q.append((sx, sy))
+    ans = 1
     while q:
         x, y = q.popleft()
-        cnt += 1
         for k in range(4):
-            nx, ny = x+dx[k], y+dy[k]
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                continue
-            if d[nx][ny] != 0:
-                continue
-            if (a[x][y] & (1 << k)) > 0:
-                continue
-            q.append((nx, ny))
-            d[nx][ny] = rooms
-    return cnt
+            if a[x][y] & (1 << k) == 0:  # 사실 이 코드가 가장 중요하다. 이렇게 simple하게 비트마스크 코드를 작성할 수 있다
+                # 왜 이게 되는지 궁금하면 , 실제로 print(6 & (1<<2)) 등을 해봐라
+                # 6의 1진수 버전에서, 해당 위치에 1이 존재한다면, 해당 숫자를 출력하고
+                # 1이 존재하지 않는다면, 0을 출력하게 된다
+                nx, ny = x + dx[k], y + dy[k]
+                if 0 <= nx < m and 0 <= ny < n:
+                    if group[nx][ny] == -1:
+                        ans += 1
+                        group[nx][ny] = room
+                        q.append((nx, ny))
+    return ans
 
 
-rooms = 0
-room = [0]
-for i in range(n):
-    for j in range(m):
-        if d[i][j] == 0:
-            rooms += 1
-            room.append((bfs(i, j, rooms)))
-print(rooms)
-ans = 0
-for i in range(1, rooms+1):
-    if ans < room[i]:
-        ans = room[i]
-print(ans)
-ans = 0
-for i in range(n):
-    for j in range(m):
-        x, y = i, j
+'''
+1) 이 성에 있는 방의 개수
+2) 가장 넓은 방의 넓이
+3) 하나의 벽을 제거하여 얻을 수 있는 가장 넓은 방의 크기
+
+서,북,동,남 --> 1,2,4,8
+만약, 비트를 통해, 비교하여, 벽이 존재하지 않는다면
+bfs를 통해, 같은 그룹으로 해주기 
+'''
+
+for i in range(m):
+    for j in range(n):
+        if group[i][j] == -1:
+            room += 1
+            res = bfs(i, j, room)
+            group_size.append(res)
+
+print(len(group_size))
+print(max(group_size))
+
+for x in range(m):
+    for y in range(n):
         for k in range(4):
             nx, ny = x+dx[k], y+dy[k]
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+            if nx < 0 or nx >= m or ny < 0 or ny >= n:
                 continue
-            if d[nx][ny] == d[x][y]:
+            if group[nx][ny] == group[x][y]:  # 그룹정보가 같으면 skip
                 continue
-            if (a[x][y] & (1 << k)) > 0:
-                if ans < room[d[x][y]]+room[d[nx][ny]]:
-                    ans = room[d[x][y]]+room[d[nx][ny]]
-print(ans)
+            s = group_size[group[nx][ny]] + group_size[group[x][y]]
+            mSize = max(mSize, s)
+
+print(mSize)
+
 
 # C++ ----
+'''
 # include <iostream>
 # include <queue>
 using namespace std
 int n, m
-int a[50][50]
-int d[50][50]
-int room[50*50]
-int dx[] = {0, -1, 0, 1}
-int dy[] = {-1, 0, 1, 0}
+int a[50][50] // 입력값 
+int d[50][50] // 방의 번호 
+int room[50*50]  // room : 1번 방의 크기 
+int dx[] = {0, -1, 0, 1}  // 여기서는 순서가 매우 중요 
+int dy[] = {-1, 0, 1, 0}  // 서,북,동,남 == 1,2,4,8
+
 int bfs(int x, int y, int rooms) {
     queue < pair < int, int >> q
     q.push(make_pair(x, y))
     d[x][y] = rooms
-    int cnt = 0
+    int cnt = 0 // 방의 크기 
     while (!q.empty()) {
         x = q.front().first
         y = q.front().second
@@ -84,15 +102,17 @@ int bfs(int x, int y, int rooms) {
             int nx = x+dx[k]
             int ny = y+dy[k]
             if (nx < 0 | | nx >= n | | ny < 0 | | ny >= m) continue
-            if (d[nx][ny] != 0) continue
-            if (a[x][y] & (1 << k)) continue
+            if (d[nx][ny] != 0) continue // 방문여부 검사 
+            if (a[x][y] & (1 << k)) continue // 벽 검사 --> 그 bit가 1로 세팅되어있다는 것은, 벽이라는 의미, 이동하지 않는다 
             q.push(make_pair(nx, ny))
             d[nx][ny] = rooms
         }
     }
     return cnt
 }
+
 int main() {
+
     cin >> m >> n
     for (int i=0
          i < n
@@ -103,6 +123,7 @@ int main() {
             cin >> a[i][j]
         }
     }
+
     int rooms = 0
     for (int i=0
          i < n
@@ -111,11 +132,13 @@ int main() {
              j < m
              j++) {
             if (d[i][j] == 0) {
+                // 방문하지 않은 칸에 대한 bfs
                 rooms += 1
                 room[rooms] = bfs(i, j, rooms)
             }
         }
     }
+
     cout << rooms << '\n'
     int ans = 0
     for (int i=1
@@ -125,6 +148,8 @@ int main() {
             ans = room[i]
         }
     }
+
+    // 벽 없애기 
     cout << ans << '\n'
     ans = 0
     for (int i=0
@@ -150,6 +175,9 @@ int main() {
             }
         }
     }
+
     cout << ans << '\n'
     return 0
 }
+
+'''
