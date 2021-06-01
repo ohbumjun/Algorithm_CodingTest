01a9782c007d136c4a5d8c5b35c9d0b31aedb48e
OH BUM JUN <beomjun0638@hanmail.net>
2021-06-01 22:57:48 +0900

    백준 큰수만들기 프로그래머스

--- Algorithm/Programmers/Python/1stAgain/큰수만들기_0601.py
+++ Algorithm/Programmers/Python/1stAgain/큰수만들기_0601.py
@@ -26,74 +26,28 @@ def solution(number, k):
     return str(threes[0])
 
 # 해결 코드
+# 특정 숫자가, 오른쪽에서 왼쪽으로 가면서, 큰 숫자만을 남기는 원리를 생각하면 된다
 
 
-def solution(name):
-    # 처음부터 각 알파벳에 대한 상하 이동 횟수를 저장한다
-    make_name = [min(ord(n)-ord('A'), ord('Z')-ord(n)+1) for n in name]
-    idx, answer = 0, 0
+def solution(number, k):
 
-    while True:
-        # 상하 이동
-        answer += make_name[idx]
-        make_name[idx] = 0
-
-        # break 조건
-        if sum(make_name) == 0:
-            break
-
-        # 좌,우 이동
-        left, right = 1, 1
-        while make_name[idx-left] == 0:
-            left += 1
-        while make_name[idx+right] == 0:
-            right += 1
-
-        answer += left if left < right else right
-        idx += -left if left < right else right
-    return answer
-
-
-'''
-자. 사실 여기서의 핵심은 
-[idx-left] 혹은 [idx+right] 하는 과정에서
-범위를 넘어서면 어떡하냐는 것이다
-
-그리고 해당 코드가 올바르게 실행되기 위해서는
-idx가 반드시 0으로 시작되어야 한다
-
-------------------------------------------
-
-1) left
-left 부터 살펴보자 
-idx-left가 - 가 되면 어떡하냐고 ?
-python 문법상 배열에서 -1,-2는
-자연스럽게 오른쪽 끝으로 이동하게 된다
-따라서 이는 문제가 없다
-
-
-2) right
-사실 이 right이 문제다
-
-왜냐하면 idx + right 이 len(name)을 벗어난다고
-0부터 시작하는 원리는 아니기 때문이다 
-
-예를 들어, 아래와 같은 문자열이 있었다고 해보자
-J A A A B A C
-
-이 경우, J를 처리한 다음
-C로 갈건데
-C에서 idx + right을 하는 순간 범위를 벗어나는 것이 아닌가 ?
-라고 생각할 수도 있다
-
-하지만, 이때의 idx 값을 살펴봐야 한다
-J에서 바로 C로 갔다는 얘기는 left로 갔다는 것이고
-0 idx에서 왼쪽으로 가면 -1 이다
-
-즉, C 에 도착했을 때 idx는 len(name) -1 이 아니라
--1 이기 때문에 idx + right을 해도 범위에서 
-벗어나는 일은 생기지 않는다는 것이다 
-------------------------------------------
-
-
-'''
+    max_nums = []
+    # 앞에서부터 탐색해가면서, 현재 만난 숫자가 더 크면, stack을 빼고, 넣어주는 과정을 반복한다
+    # 뺄때마다 k를 감소시켜준다
+    # k가 0이 되면 max_nums 배열 + number 배열 안에 있는 숫자들을 return
+    # k가 0 이 되지 않은 상태에서, 모든 숫자를 순회하면 남은 k만큼 뒤에서 빼준다
+    for i in range(len(number)):
+        allRemv = False
+        if not max_nums:
+            max_nums.append(number[i])
+            continue
+        while max_nums and max_nums[-1] < number[i]:
+            max_nums.pop()
+            k -= 1
+            if k == 0:
+                max_nums += number[i:]
+                return ''.join(max_nums)
+        max_nums.append(number[i])
+    for i in range(k):
+        max_nums.pop()
+    return ''.join(max_nums)
