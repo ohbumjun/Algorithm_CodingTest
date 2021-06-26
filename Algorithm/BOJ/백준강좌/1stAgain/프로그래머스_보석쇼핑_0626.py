d5e8711a908b03a3093b315e2ca20a1fa28576e1
OH BUM JUN <beomjun0638@hanmail.net>
2021-06-26 15:52:51 +0900

    프로그래머스 보석쇼핑

---
+++ Algorithm/BOJ/백준강좌/1stAgain/프로그래머스_보석쇼핑_0626.py
@@ -0,0 +1,38 @@
+# https://programmers.co.kr/learn/courses/30/lessons/67258
+
+# 투포인터 알고리즘
+from collections import defaultdict
+
+
+def solution(gems):
+    # 투포인터 알고리즘
+    # 시작, 끝
+    st, ed = 0, 0
+    # break 조건
+    # - ed가 len + 1 이 될 때(범위 : ed  - 1 까지 )
+    # - st가 len 이 될 때
+    ans, info = [0, int(1e9)], dict()
+    l_all = len(set(gems))
+    info[gems[st]] = 1
+    l_gems = len(gems)
+    while True:
+        # info가 모두 포함하는지 확인하기
+        if len(info.keys()) == l_all:
+            if ed - st + 1 < ans[1] - ans[0] + 1:
+                ans = [st, ed]
+            info[gems[st]] -= 1
+            if info[gems[st]] == 0:
+                del info[gems[st]]
+            st += 1
+            if st == l_gems:
+                break
+        elif len(info.keys()) < l_all:
+            ed += 1
+            if ed == l_gems:
+                break
+            if gems[ed] not in info:
+                info[gems[ed]] = 1
+            else:
+                info[gems[ed]] += 1
+
+    return [ans[0]+1, ans[1]+1]
