2eb0080d1382127a2539b4638e119ed6551d8445
OH BUM JUN <beomjun0638@hanmail.net>
2021-08-07 13:53:52 +0900

    다단계 칫솔판매

--- Algorithm/BOJ/백준강좌/1stAgain/프로그래머스_다단계칫솔판매_0807.py
+++ Algorithm/BOJ/백준강좌/1stAgain/프로그래머스_다단계칫솔판매_0807.py
@@ -1,6 +1,6 @@
 # https://programmers.co.kr/learn/courses/30/lessons/77486
 
-# 첫번째 풀이 (시간초과) ---------------------------------
+# 첫번째 풀이 (시간초과) ----------------------------------
 from collections import defaultdict
 
 
@@ -31,6 +31,42 @@ def solution(enroll, referral, seller, amount):
                 my_inc = income
             d[seller_p][1] += my_inc
             # 추천인 이익 처리
+            inv_p = d[seller_p][0]
+            if inv_p == '-':
+                break
+            # 정보 update
+            seller_p = inv_p
+            income = up_inc
+    res = []
+    for i in range(enroll_len):
+        res.append(d[enroll[i]][1])
+    return res
+
+
+# 두번째 풀이 -------------------------------------
+
+
+def solution(enroll, referral, seller, amount):
+    d = defaultdict(list)
+    enroll_len = len(enroll)
+    for i in range(enroll_len):
+        d[enroll[i]] = [referral[i], 0]
+    seller_len = len(seller)
+    for i in range(seller_len):
+        seller_p = seller[i]
+        income = amount[i] * 100
+        while True:
+            # 금액 계산
+            up_inc = int(income*0.1)
+            my_inc = income - up_inc
+            # 자기 이익 처리
+            if up_inc < 1:
+                my_inc = income
+            d[seller_p][1] += my_inc
+            # 추천인 이익 처리
+            # ---------------------> 추가한 한 줄 (사실 그렇게 중요한 건지는 모르겠다. 어차피 한번만 더 탐색되는 개념 아닌가 ?? )
+            if up_inc == 0:
+                break
             inv_p = d[seller_p][0]
             if inv_p == '-':
                 break
