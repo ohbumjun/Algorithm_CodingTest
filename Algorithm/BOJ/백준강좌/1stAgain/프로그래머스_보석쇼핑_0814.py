91432ceee6e7c662e73bd3c4fc526b35fa479666
OH BUM JUN <beomjun0638@hanmail.net>
2021-08-14 10:33:28 +0900

    보석쇼핑 2번째

--- Algorithm/BOJ/백준강좌/1stAgain/프로그래머스_보석쇼핑_0814.py
+++ Algorithm/BOJ/백준강좌/1stAgain/프로그래머스_보석쇼핑_0814.py
@@ -4,7 +4,56 @@
 from collections import defaultdict
 
 # 완전탐색으로 하면 정답은 구하지만, 틀리게 될 것이다
-#
+
+# 첫번째 풀이 : set 이용 --> 시간초과
+from collections import defaultdict
+
+
+def solution(gems):
+    ans = []
+    len_gem = len(gems)
+    # 보석들 종류의 갯수를 알아내야 한다
+    set_gems = set(gems)
+    prec_len = len(set_gems)
+
+    # 투포인터 알고리즘 --> 시작,시작+1 위치 잡기 ( st ~ ed-1)
+    # 만약 개수가 작으면 ed += 1
+    # 만약 개수가 같다면
+    # 범위가 같다면, 그냥 ans 배열에 추가
+    # 만약 범위가 더 작다면, ans 초기화 후에 배열에 추가
+    st, ed = 0, 1
+    p_gems = gems[st:ed]
+    minLen = int(1e9)
+
+    while True:
+        if st > ed:
+            break
+        if ed >= len_gem + 1:
+            break
+        if st >= len_gem:
+            break
+        len_p_prec = len(set(p_gems))
+        # 보석 길이 비교
+        if len_p_prec < prec_len:
+            if ed >= len_gem:
+                break
+            p_gems.append(gems[ed])
+            ed += 1
+        if len_p_prec == prec_len:
+            len_range = ed - st
+            if len_range == minLen:
+                ans.append((st+1, ed+1))
+            if len_range < minLen:
+                minLen = len_range
+                ans = [(st+1, ed+1)]
+            p_gems.pop(0)
+            st += 1
+
+    ans.sort()
+    print(ans)
+    return (ans[0][0], ans[0][1]-1)
+
+# Dictionary 이용 --------------------------------
 
 
 def solution(gems):
