b8b261ffd5828fc4dae3be7c420e60b9b7c24d26
OH BUM JUN <beomjun0638@hanmail.net>
2021-10-14 10:58:03 +0900

    백준 센서

---
+++ Algorithm/BOJ/백준강좌/1stAgain/센서_1014.py
@@ -0,0 +1,21 @@
+# https://www.acmicpc.net/status?user_id=dhsys112&problem_id=2212&from_mine=1
+
+import sys
+
+n = int(sys.stdin.readline())
+k = int(sys.stdin.readline())
+pos = sorted(list(map(int, sys.stdin.readline().split())))
+
+if k >= n:
+    print(0)
+    sys.exit()
+
+dist = []
+for i in range(1, n):
+    dist.append(pos[i] - pos[i-1])
+
+dist.sort(reverse=True)
+for _ in range(k-1):
+    dist.pop(0)
+
+print(sum(dist))
