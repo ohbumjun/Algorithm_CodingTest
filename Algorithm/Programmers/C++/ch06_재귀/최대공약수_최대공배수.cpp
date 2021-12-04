f89c847423acd7d16cadba578aebd2398bc76a1a
ohbumjun <beomjun0638@hanmail.net>
2021-12-04 12:57:03 +0900

    백준 중량제한

--- Algorithm/Programmers/C++/ch06_재귀/최대공약수_최대공배수.cpp
+++ Algorithm/Programmers/C++/ch06_재귀/최대공약수_최대공배수.cpp
@@ -3,12 +3,12 @@
 
 // 최대 공약수
 // 유클리드 알고리즘 ( 유클리드 호제법 )
-int gcd(int a,int b)
+int gcd(int a, int b)
 {
     if (b == 0)
         return a;
     else
-        return gcd(b, a%b);
+        return gcd(b, a % b);
 };
 
 // 최소 공배수
@@ -16,7 +16,7 @@ int gcd(int a,int b)
 // a * b ==  gcd(a,b) * lcm(a,b);
 int lcm(int a, int b)
 {
-    return (a * b) / gcd(a,b);
+    return (a * b) / gcd(a, b);
 }
 
 // 최대 공약수, 최소 공배수 계산함수
@@ -30,10 +30,10 @@ constexpr std::common_type<M,N> lcm(M m, N n);
 
 int main()
 {
-    int gcd1 = gcd(10,15);
-    int lcm1 = lcm(10,15);
-    
-    int gcd2 = std::gcd(10,15);
-    int lcm2 = std::lcm(10,15);
+    int gcd1 = gcd(10, 15);
+    int lcm1 = lcm(10, 15);
+
+    int gcd2 = std::gcd(10, 15);
+    int lcm2 = std::lcm(10, 15);
     return 0;
 }
