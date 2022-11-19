80b714d66f414d861cf80efe80a505552b17e9ce
ohbumjun <beomjun0638@hanmail.net>
2022-11-19 14:00:33 +0900

    백준 사회망서비스 C++

--- Algorithm/BOJ/백준/Python/Blog/트리의지름221117.py
+++ Algorithm/BOJ/백준/Python/Blog/트리의지름221117.py
@@ -50,3 +50,145 @@ for i in range(len(ch)):
     ch[i] = 0
 print("N", D)
 print(bfs(N)[1])
+
+
+'''
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <string>
+#include <set>
+#include <queue>
+#include <math.h>
+#include <algorithm>
+#include <unordered_map>
+
+#define endl "\n"
+#define INT_MAX int(1e9)
+
+using namespace std;
+
+int V;
+vector<vector<pair<int, int>>> vecGraph;
+vector<bool> vecCheck;
+vector<int>   vecDist;
+
+int maxDistToLast = 0;
+int maxLastNode = -1;
+
+void Input()
+{
+    cin >> V;
+
+    vecGraph.resize(V + 1);
+    vecDist = vector<int>(V + 1, INT_MAX);
+    vecCheck = vector<bool>(V + 1, false);
+
+    for (int v = 0; v < V; ++v)
+    {
+        int st;
+        cin >> st;
+        while (true)
+        {
+            int nxt, cost;
+            cin >> nxt;
+            if (nxt == -1)
+                break;
+            cin >> cost;
+            vecGraph[st].push_back(make_pair(nxt, cost));
+            vecGraph[nxt].push_back(make_pair(st, cost));
+        }
+    }
+};
+
+void Dijkstra(int stNode)
+{
+    // curNode, accDist
+    priority_queue<pair<int, int>> Queue;
+    Queue.push(make_pair(stNode, 0));
+    vecCheck[stNode] = 0;
+
+    while (!Queue.empty())
+    {
+        auto [curNode, accDist] = Queue.top();
+        Queue.pop();
+
+        if (vecDist[curNode] < accDist)
+            continue;
+
+        vecDist[curNode] = accDist;
+
+        if (vecCheck[curNode])
+            continue;
+
+        vecCheck[curNode] = true;
+
+        for (int i = 0; i < vecGraph[curNode].size(); ++i)
+        {
+            int nxtN = vecGraph[curNode][i].first;
+            int nDist = vecGraph[curNode][i].second;
+
+            if (vecCheck[nxtN])
+                continue;
+
+            Queue.push(make_pair(nxtN, accDist + nDist));
+        }
+    }
+};
+
+void Solve()
+{
+    Dijkstra(1);
+
+    for (int i = 1; i <= V; ++i)
+    {
+        if (vecDist[i] > maxDistToLast)
+        {
+            maxDistToLast = vecDist[i];
+            maxLastNode = i;
+        }
+    }
+
+    // cout << "maxDistToLast, maxLastNode : " << maxDistToLast << "," << maxLastNode << endl;
+
+    vecDist = vector<int>(V + 1, INT_MAX);
+    vecCheck = vector<bool>(V + 1, false);
+
+    Dijkstra(maxLastNode);
+
+    maxDistToLast = 0;
+    maxLastNode = -1;
+
+    for (int i = 1; i <= V; ++i)
+    {
+        if (vecDist[i] > maxDistToLast)
+        {
+            maxDistToLast = vecDist[i];
+            maxLastNode = i;
+        }
+    }
+
+    // cout << "maxDistToLast, maxLastNode : " << maxDistToLast << "," << maxLastNode << endl;
+
+    cout << maxDistToLast << endl;
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    freopen("input_c.txt", "r", stdin);
+
+    Input();
+
+    Solve();
+    
+    return 0;
+}
+
+
+'''
