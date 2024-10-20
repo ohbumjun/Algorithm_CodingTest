c6ebbbf71c2c3b146876eab1573cca980020542b
ohbumjun <beomjun0638@hanmail.net>
2024-10-20 20:22:22 +0900

    가장 긴 증가하는 부분수열 2 cpp

--- Algorithm/BOJ/백준/Python/2024다시풀것들/가장긴증가하는부분수열2_241001.py
+++ Algorithm/BOJ/백준/Python/2024다시풀것들/가장긴증가하는부분수열2_241001.py
@@ -9,7 +9,6 @@ sys.setrecursionlimit(10000) ##
 def findPlace(tmp, val):
     lt = 1 # 어차피 tmp 맨 앞 0은 고려하지 않기 때문이
     rt = len(tmp) -1
-    res = 0
 
     # 기존 이진 탐색과 다소 다르다
     # 일반적으로 이진 탐색은 tmp[mid] == val을 향해 찾아가지만
@@ -48,4 +47,104 @@ if __name__ == "__main__" :
 
     print(len(tmp) - 1)
             
+###
+// https://www.codetree.ai/training-field/frequent-problems/problems/hide-and-seek?page=4&pageSize=5
 
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <thread>
+#include <cassert>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <set>
+#include <queue>
+#include <math.h>
+#include <stack>
+#include <algorithm>
+#include <unordered_map>
+#include <sstream>
+
+#define endl "\n"
+#define INT_MAX int(1e9)
+#define MAX 10001
+
+// #define DEBUG 1
+
+using namespace std;
+int N;
+vector<int> nums;
+vector<int> LIS;
+
+void Input()
+{
+	cin >> N;
+	nums.resize(N);
+
+	for (int n = 0; n < N; ++n)
+		cin >> nums[n];
+}
+
+
+void Solve()
+{
+	LIS.push_back(nums[0]);
+	for (int i = 1; i < N; ++i)
+	{
+		if (LIS.back() < nums[i])
+		{
+			LIS.push_back(nums[i]);
+		}
+		else
+		{
+			// LIS 에서 현재 원소가 들어갈 위치를 찾아서 넣어준다.
+			// 단, 같은 원소는 ...?
+			int insertIdx = -1;
+			int left = 0;
+			int right = LIS.size() - 1;
+
+			while (left < right)
+			{
+				int midIdx = (left + right) / 2;
+
+				// 더 작은 원소라면 오른쪽 탐색
+				if (LIS[midIdx] < nums[i])
+				{
+					left = midIdx + 1;
+				}
+				else if (LIS[midIdx] > nums[i])
+				{
+					right = midIdx;
+				}
+				else if (LIS[midIdx] == nums[i])
+				{
+					left = midIdx;
+					break;
+				}
+			}
+
+			LIS[left] = nums[i];
+		}
+	}
+
+	cout << LIS.size() << endl;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	freopen("input_c.txt", "r", stdin);
+
+	Input();
+
+	Solve();
+
+	return 0;
+}
+
+
+###
