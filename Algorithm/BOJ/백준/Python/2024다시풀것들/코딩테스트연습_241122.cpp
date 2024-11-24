b058776eb72ee11aa7f007270a5adebefee63996
ohbumjun <beomjun0638@hanmail.net>
2024-11-24 22:30:26 +0900

    평범한 배낭 cpp

--- Algorithm/BOJ/백준/Python/2024다시풀것들/코딩테스트연습_241122.cpp
+++ Algorithm/BOJ/백준/Python/2024다시풀것들/코딩테스트연습_241122.cpp
@@ -1,57 +1,78 @@
-// https://school.programmers.co.kr/learn/courses/30/lessons/118668
-
 #include <string>
 #include <vector>
-#define MX 999999999
-using namespace std;
-// dp의 최대 index 값은 150(req)+30(rwd)이다.
-// 150으로 줘서 segmentation fault가 발생했다.
-vector<vector<int>> dp(201, vector<int>(201, MX));
+#include <queue>
 
-int solution(int alp, int cop, vector<vector<int>> problems) 
+using namespace std;
+
+
+int solution(int stAlg, int stCod, vector<vector<int>> problems) 
 {
-    int answer = 0, alp_mx=0, cop_mx=0;
+    int answer = 0;
     
-    for(int i=0; i<problems.size(); i++)
+    int algReq = 0, codReq = 0;
+    
+    // 모든 문제 풀 수 있는 알고력, 코딩력 구하기
+    for (const vector<int>& prob : problems)
     {
-        alp_mx=max(alp_mx, problems[i][0]);
-        cop_mx=max(cop_mx, problems[i][1]);
+        algReq = algReq < prob[0] ? prob[0] : algReq;
+        codReq = codReq < prob[1] ? prob[1] : codReq;
     }
     
-    // 예외 처리
-    alp=min(alp, alp_mx), cop=min(cop, cop_mx);
-    dp[alp][cop] = 0;
-    int x_idx, y_idx;
-    
-    for(int i=alp; i<=alp_mx; i++)
+    if (algReq <= stAlg && codReq <= stCod)
     {
-        for(int j=cop; j<=cop_mx; j++)
+        return 0;
+    }
+    
+    // 5.1 vs 3.3
+    algReq = stAlg > algReq ? stAlg : algReq;
+    codReq = stCod > codReq ? stCod : codReq;
+    
+    // dp[알고력][코딩력] : 해당 알고력, 코딩력을 가지기 까지 걸리는 최소 시간
+    vector<vector<int>> dpValue(algReq + 1, vector<int>(codReq + 1, int(1e9)));
+    
+    dpValue[stAlg][stCod] = 0;
+    
+    for (int alg = stAlg; alg <= algReq; alg++)
+    {
+        for (int cod = stCod; cod <= codReq; cod++)
         {
-        	// 예외 처리
-            x_idx=min(i+1, alp_mx), y_idx=min(j+1, cop_mx);
-            
-            // CASE 1. 알고력 1 증가
-            dp[x_idx][j]=min(dp[x_idx][j], dp[i][j]+1);
-            
-            // CASE 2. 코딩력 1 증가
-            dp[i][y_idx]=min(dp[i][y_idx], dp[i][j]+1);
-            
-            // CASE 3. 문제 풀이
-            // for(int k=0; k<problems.size(); k++){
+            // 알고리즘 공부로 알고력 상승
+            if (alg + 1 <= algReq)
+            {
+                dpValue[alg + 1][cod] = min(dpValue[alg + 1][cod], dpValue[alg][cod] + 1);
+            }
+    
+            // 코딩 공부로 코딩력 상승
+            if (cod + 1 <= codReq)
+            {
+                dpValue[alg][cod + 1] = min(dpValue[alg][cod + 1], dpValue[alg][cod] + 1);
+            }
+
+            // 문제 풀어서 상승
             for (const vector<int>& prob : problems)
             {
-                int alp_req=prob[0], cop_req=prob[1];
-                int alp_rwd=prob[2], cop_rwd=prob[3], cost=prob[4];
-            
-                if(i<alp_req || j<cop_req) continue;
+                if (alg < prob[0])
+                    continue;
+                if (cod < prob[1])
+                    continue;
                 
-                // 예외 처리
-                x_idx=min(i+alp_rwd, alp_mx);
-                y_idx=min(j+cop_rwd, cop_mx);
+                int algRet = prob[2];
+                int codRet = prob[3];
+                int cost = prob[4];
                 
-                dp[x_idx][y_idx]=min(dp[x_idx][y_idx], dp[i][j]+cost);
+                int nxtAlg = alg + algRet;
+                int nxtCod = cod + codRet;
+                
+                // 여기서 중요한 개념 : nxtAlg, nxtCod 가 algReq, codReq 를 넘으면
+                // algReq, codReq 으로 nxtAlg, nxtCod 를 맞추면 된다.
+                // 결국 nxtAlg, nxtCod 까지의 최단 시간이나, algReq, codReq 까지의 최단 시간이나
+                // 동일한 값을 가지게 되기 때문이다.
+                
+                dpValue[min(algReq, nxtAlg)][min(codReq, nxtCod)] = min(
+                    dpValue[min(algReq, nxtAlg)][min(codReq, nxtCod)], dpValue[alg][cod] + cost);
             }
         }
     }
-    return answer=dp[alp_mx][cop_mx];
+    
+    return answer = dpValue[algReq][codReq];
 }
